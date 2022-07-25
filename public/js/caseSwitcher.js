5fc78b0ce185d4e15f3bba2a7d6818ebdb6def51
James Tierney <james.tierney384@gmail.com>
2022-07-25 13:23:03 +0100

    edits

---
+++ public/js/caseSwitcher.js
@@ -0,0 +1,22 @@
+let para = document.getElementById('patrick')
+let txt = para.innerText
+
+const qButton = document.getElementById('quiet-btn')
+const lButton = document.getElementById('loud-btn')
+const sButton = document.getElementById('sarc-btn')
+
+qButton.addEventListener('click', lowerCase)
+
+function lowerCase(e){
+    para.innerText = txt.toLowerCase()
+}
+
+lButton.addEventListener('click', upperCase)
+
+function upperCase(u){
+    para.innerText = txt.toUpperCase()
+}
+
+sButton.addEventListener('click', sarcCase)
+
+function sarcCase(s)
